99569fabcbffdcf466810e7a373f417f95caedba
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-02 15:57:44 -0300

    cambios

---
+++ microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/MicroserviciosItemApplication.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.microserviciositem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosItemApplication.class, args);
+	}
+
+}
---
+++ microservicios-item/src/test/java/com/svillanueva/springboot/microserviciositem/MicroserviciosItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.microserviciositem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
