1425ff2269a09797e000e02cec18452dd25e30a5
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-02 15:27:53 -0300

    microservicios

---
+++ microservicios-productos/src/main/java/com/svillanueva/springboot/microserviciosproductos/MicroserviciosProductosApplication.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.microserviciosproductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosProductosApplication.class, args);
+	}
+
+}
---
+++ microservicios-productos/src/test/java/com/svillanueva/springboot/microserviciosproductos/MicroserviciosProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.microserviciosproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
