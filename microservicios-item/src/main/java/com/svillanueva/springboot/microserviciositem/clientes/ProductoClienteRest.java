ef1bd8cd6289bf632759b06dae524d9b27a3e86a
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-02 18:42:40 -0300

    cambios 2

--- microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/clientes/ProductoClienteRest.java
+++ microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/clientes/ProductoClienteRest.java
@@ -2,13 +2,15 @@ package com.svillanueva.springboot.microserviciositem.clientes;
 
 import java.util.List;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.svillanueva.springboot.microserviciositem.models.Producto;
 
-@FeignClient(name = "servicio-producto" /* , url = "http://localhost:8081" */ , path = "api/productos")
+@FeignClient(name = "servicio-producto"/* , url = "${servicio-producto.ribbon.listOfServers}" */, path = "api/productos")
+@RibbonClient(name = "servicio-producto")
 public interface ProductoClienteRest {
   @GetMapping
   List<Producto> findByAll();
