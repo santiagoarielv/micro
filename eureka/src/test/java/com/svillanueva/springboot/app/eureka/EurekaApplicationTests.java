0dae0bfad781575217518e18a32dba340e17f7ca
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-04 11:16:29 -0300

    agregando eureka

---
+++ eureka/src/test/java/com/svillanueva/springboot/app/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.app.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
