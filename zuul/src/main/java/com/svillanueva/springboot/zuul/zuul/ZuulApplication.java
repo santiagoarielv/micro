fa068aae7530cf939488577df080f0cfd415ea5a
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-05 11:41:24 -0300

    dejando maven wrapper en la carpeta raiz

---
+++ zuul/src/main/java/com/svillanueva/springboot/zuul/zuul/ZuulApplication.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.zuul.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
