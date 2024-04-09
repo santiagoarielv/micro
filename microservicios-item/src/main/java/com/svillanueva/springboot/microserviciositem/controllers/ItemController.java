f8f1e1be6e8d317b4e7f2373e32555f5239ce8d4
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-08 22:05:47 -0300

    agregando servidor de configuracion

--- microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/controllers/ItemController.java
+++ microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/controllers/ItemController.java
@@ -7,7 +7,11 @@ import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +27,13 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {
-
   private final static Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+  @Autowired
+  private Environment env;
+
+  @Value("${configuracion.texto}")
+  private String texto;
 
   @Autowired
   private CircuitBreakerFactory breakerFactory;
@@ -84,4 +93,19 @@ public class ItemController {
 
     return CompletableFuture.supplyAsync(() -> item);
   }
+
+  @GetMapping("/obtener-config")
+  public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+    logger.info(texto);
+    Map<String, String> json = new HashMap<>();
+    json.put("texto", texto);
+    json.put("puerto", puerto);
+
+    if (env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")) {
+      // json.put("autor.nombre", puerto)
+    }
+
+    return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+  }
+
 }
