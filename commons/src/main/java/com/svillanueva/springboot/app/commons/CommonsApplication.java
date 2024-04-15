7db2a71a8dcf0338e3501f2fead5e642c62feadf
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-15 13:34:11 -0300

    cambios common

--- commons/src/main/java/com/svillanueva/springboot/app/commons/CommonsApplication.java
+++ commons/src/main/java/com/svillanueva/springboot/app/commons/CommonsApplication.java
@@ -1,8 +1,10 @@
 package com.svillanueva.springboot.app.commons;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class CommonsApplication {
 }
--- microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/MicroserviciosItemApplication.java
+++ microservicios-item/src/main/java/com/svillanueva/springboot/microserviciositem/MicroserviciosItemApplication.java
@@ -1,13 +1,16 @@
 package com.svillanueva.springboot.microserviciositem;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EntityScan({ "com.svillanueva.springboot.app.commons.models.entity" })
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
 public class MicroserviciosItemApplication {
 
 	public static void main(String[] args) {
