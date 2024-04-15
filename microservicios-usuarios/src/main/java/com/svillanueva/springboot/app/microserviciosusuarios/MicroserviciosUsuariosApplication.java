f3745a415167ad7d915736266295be8e3cc98ca9
Santiago Ariel <santiagoarielv98@gmail.com>
2024-04-15 13:41:09 -0300

    cambio & agregando microservicio usuarios

---
+++ microservicios-usuarios/src/main/java/com/svillanueva/springboot/app/microserviciosusuarios/MicroserviciosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.svillanueva.springboot.app.microserviciosusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosUsuariosApplication.class, args);
+	}
+
+}
